6dd4fe2fadeed7194c3fcce2d46aacfdbf00e8b3
senhmane <alexgera05@gmail.com>
2025-12-25 22:55:36 +0500

    new2

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world", add(5, 6))
+	fmt.Println("hello world", add(5, 6), sub(5, 4))
 
 }
 
 func add(a, b int) int {
 	return a + b
+}
+func sub(a, b int) int {
+	return a - b
 }
